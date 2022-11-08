4224b1ab1fd25092295ede319adad6fe1a0d6d89
4o4E <869951226@qq.com>
2022-11-08 19:59:03 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # zjooc-script
+
+自动挂机刷[zjooc](https://www.zjooc.cn/)的[油猴](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)脚本, 目前支持视频学习(自动静音+4倍速)和文档学习
+
+## 可配置项
+
+```javascript
+// 设置, 根据自己的网络状况调整
+const config = {
+  // 首次学习开始之前的延迟(等待html加载)
+  start: 1000,
+  // 点击链接跳转到下一学习后等待加载的时长
+  next: 1000,
+  // 跳转后开始前的等待时长
+  before: 1000
+}
+```
+
+## PS
+
+练手作, 本来想让脚本支持多开的同时刷的, 结果发现比较麻烦...
+
+遇到问题可以在(issue)[https://github.com/4o4E/zjooc-script/issues]发
---
+++ zjooc.js
@@ -0,0 +1,114 @@
+// ==UserScript==
+// @name         zjooc
+// @namespace    https://github.com/4o4E/zjooc-script
+// @version      1.0.0
+// @description  zjooc 自动刷题
+// @author       404E
+// @match        *://www.zjooc.cn/ucenter/student/course/study/*/plan/detail/*
+// @grant        none
+// @supportURL   https://github.com/4o4E/zjooc-script/issues
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // 设置, 根据自己的网络状况调整
+  const config = {
+    // 首次学习开始之前的延迟(等待html加载)
+    start: 1000,
+    // 点击链接跳转到下一学习后等待加载的时长
+    next: 1000,
+    // 跳转后开始前的等待时长
+    before: 1000
+  }
+
+  function getCurrent() {
+    return $(".fr .el-tabs__nav-scroll .el-tabs__nav .is-active span > span")[0].innerHTML;
+  }
+
+  // 前往下一个学习
+  function next() {
+    // 当前播放列表
+    let next = $(".fr .el-tabs__nav-scroll .el-tabs__nav").find(".is-active + .el-tabs__item.is-top").first();
+    if (next.length != 0) {
+      console.log("[zjooc] > 跳转至下一项: " + next.text());
+      next.click();
+      return;
+    }
+
+    // 当前节
+    next = $(".base-asider .el-submenu.is-active ul > li + li").first();
+    if (next.length != 0) {
+      console.log("[zjooc] > 跳转至下一节: " + next.text());
+      next.click();
+      return;
+    }
+
+    // 下一章
+    next = $(".base-asider .el-submenu.is-active + li").find("ul > li").first();
+    if (next.length != 0) {
+      console.log("[zjooc] > 跳转至下一章: " + next.text());
+      next.click();
+      return;
+    }
+    alert("已完成所有学习");
+    return;
+  }
+
+  // 尝试完成文档类型的学习
+  function tryDoc() {
+    console.log("[zjooc] > 开始文档学习: " + getCurrent());
+    // 点击完成学习按钮
+    $(".el-main .el-button")[0].click();
+    // 等待跳转
+    setTimeout(() => {
+      // 学习完成
+      console.log("[zjooc] > 文档学习完成: " + getCurrent());
+      next();
+      setTimeout(start, config.before);
+    }, config.next);
+  }
+
+  // 视频设置点击
+  function configVideo() {
+    // 静音
+    $("video")[0].parentElement.children[2].children[18].click();
+    // 4倍速
+    $("video")[0].parentElement.children[8].children[0].click();
+    // 开始播放
+    $("video")[0].parentElement.children[2].children[0].click();
+  }
+
+  // 尝试完成视频学习
+  function tryVideo() {
+    console.log("[zjooc] > 开始视频学习: " + getCurrent());
+    configVideo();
+    // let b = false;
+    // 循环检测是否结束
+    let id = setInterval(() => {
+      let arr = $("video")[0].parentElement.children[2].children[7].innerHTML.split(' / ');
+      // 若视频未开始播放则修复
+      if (arr[0] === "00:00") configVideo();
+      if (arr[0] === arr[1]) {
+        // 播放完成
+        console.log("[zjooc] > 视频播放完成: " + getCurrent());
+        // 取消定时器
+        clearInterval(id);
+        // 下一个
+        next();
+        setTimeout(start, config.before);
+        return;
+      }
+    }, 250);
+  }
+
+  // 检测学习类型并开始学习
+  function start() {
+    if ($("video").length != 0) tryVideo();
+    else tryDoc();
+  }
+
+  setTimeout(() => {
+    start();
+  }, config.start);
+})();
